61f400e8545eafeff91bb23e0a99853b6889c23b
Will <doggy.huang@gmail.com>
2020-10-15 10:50:24 +0800

    建立 ccmask Pipe 元件

---
+++ src/app/ccmask.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CcmaskPipe } from './ccmask.pipe';
+
+describe('CcmaskPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CcmaskPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
