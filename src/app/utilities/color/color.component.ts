166f70794307459a229a7b971d97294077f3dd71
Will <doggy.huang@gmail.com>
2020-10-15 16:28:16 +0800

    練習從元件取得參數 (paramMap, queryParamMap)

--- src/app/utilities/color/color.component.ts
+++ src/app/utilities/color/color.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   templateUrl: './color.component.html',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorComponent implements OnInit {
 
-  constructor() { }
+  name = '';
+  type = 0;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // const p: Params = this.route.snapshot.params;
+    // const type = p['type'];
+
+    // this.type = +this.route.snapshot.paramMap.get('type');
+    this.route.paramMap.subscribe(param => {
+      this.type = +param.get('type');
+    });
+
+    // this.name = this.route.snapshot.queryParamMap.get('name');
+    this.route.queryParamMap.subscribe(param => {
+      this.name = param.get('name');
+    });
   }
 
 }
