6654ff6b26df9d70ba471dd5afc292019f064b6a
Will <doggy.huang@gmail.com>
2020-10-20 11:11:14 +0800

    示範透過 HttpClient 取得 AuthGuard 的驗證狀態

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,18 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem('token')) {
       return true;
+      // return this.http.get('').pipe(map((x: any) => {
+      //   if (x.role === 'admin') {
+      //     return this.router.parseUrl('/admin');
+      //   } else {
+      //     return this.router.parseUrl('/');
+      //   }
+      // }));
     } else {
       return this.router.parseUrl('/login?returnUrl=' + state.url);
     }
